418398f26188050529b6edfd5f9721ea33161f5d
hiroakioishi <irishoak.0610@gmail.com>
2024-08-27 17:00:14 +0900

    html, css修正

--- js/main.js
+++ js/main.js
@@ -1,47 +1,49 @@
-$(document).ready(function() {
-    $.getJSON('./js/data.json', function(data) {
-        // JSONデータの配列をループ
-        $.each(data, function(index, item) {
-            // メインの正方形の画像がある場合のみ生成
-            var keyVisual = item.keyvisual ? `<img src="./img/${item.keyvisual}" alt="メイン画像" class="main-images">` : '';
+$(document).ready(function(){
+	$.getJSON('./js/data.json', function(data){
+		// JSON配列をループ
+		$.each(data, function(index, item){
+			// メインビジュアルがある場合のみ生成
+            var keyVisual = item.keyvisual ? `<img src="./img/${item.keyvisual}" alt="mainvisual" class="main-images">` : '';
 
-            // 小さな正方形の画像がある場合のみ生成
-            var subVisual1 = item.subvisual01 ? `<img src="./img/${item.subvisual01}" alt="サブビジュアル1">` : '';
-            var subVisual2 = item.subvisual02 ? `<img src="./img/${item.subvisual02}" alt="サブビジュアル2">` : '';
-            var subVisual3 = item.subvisual03 ? `<img src="./img/${item.subvisual03}" alt="サブビジュアル3">` : '';
-            var subVisual4 = item.subvisual04 ? `<img src="./img/${item.subvisual04}" alt="サブビジュアル4">` : '';
+            // サブビジュアルがある場合のみ生成
+            var subVisual1 = item.subvisual01 ? `<img src="./img/${item.subvisual01}" alt="subvisual1">` : '';
+            var subVisual2 = item.subvisual02 ? `<img src="./img/${item.subvisual02}" alt="subvisual2">` : '';
+            var subVisual3 = item.subvisual03 ? `<img src="./img/${item.subvisual03}" alt="subvisual3">` : '';
+            var subVisual4 = item.subvisual04 ? `<img src="./img/${item.subvisual04}" alt="subvisual4">` : '';
+		
+			var contentDiv = `
+				<div class="cotent-index"> ${zeroPad(item.id+1)} </div>
+				<div class="content">
+					<div class="images">
+						<div class="key-visual">
+							${keyVisual}
+						</div>
+						<div class="sub-visuals">
+							${subVisual1}
+							${subVisual2}
+							${subVisual3}
+							${subVisual4}
+						</div>
+					</div>
+					<div class="text">
+						<p><span class="school">${item.school} ${item.department} ${item.seminar}</span></p>                  
+						<p><span class="creator">${item.name}</span></p>
+						<p>
+							<span class="title">${item.title}</span>
+							<span class="duration">&nbsp;&nbsp;(${Math.floor(item.videoduration / 60)}分${item.videoduration % 60}秒)</span>
+						</p>
+						<p class="othercredits">${item.othercredits}</p>
+						<p> </p>
+						<p>${item.description}</p>
+					</div>
+				</div>
+			`;
 
-            // HTMLのtableに追加する新しい行を生成
-            var tableRow = `
-                <tr class="art-table-row">
-                    <td class="left-column">
-                        <div class="key-visual">
-                            ${keyVisual}
-                        </div>
-                        <div class="sub-visuals">
-                            ${subVisual1}
-                            ${subVisual2}
-                            ${subVisual3}
-                            ${subVisual4}
-                        </div>
-                    </td>
-                    <td class="right-column">
-                        <p><span class="school">${item.school} ${item.department} ${item.seminar}</span></p>                  
-                        <p><span class="creator">${item.name}</span></p>
-                        <p>
-                            <span class="title">${item.title}</span>
-                            <span class="duration">(${Math.floor(item.videoduration / 60)}分${item.videoduration % 60}秒)</span>
-                        </p>
-                        <p> </p><br/>
-                        <p class="othercredits">${item.othercredits}</p>
-                        <p> </p>
-                        <br />
-                        <p>${item.description}</p>
-                    </td>              
-                </tr>
-            `;
-            // 生成した行をtableに追加
-            $('.art-table').append(tableRow);
-        });
-    });
+			$('#container').append(contentDiv);
+		});
+	});
 });
+
+function zeroPad(number) {
+    return number.toString().padStart(2, '0');
+}
